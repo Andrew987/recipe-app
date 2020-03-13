3396cfcffdba809777f0483edb1e30e4b9848cf5
Andras Urge <andris@mail.muni.cz>
2020-03-13 12:57:04 +0100

    Initialise repository with Spring Boot project

---
+++ src/test/java/cz/mailmuni/andirs/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.mailmuni.andirs.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
